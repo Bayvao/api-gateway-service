eee5c188ff226e40c982c705dcf7660a9b187d7e
Bayvao Verma <34921943+Bayvao@users.noreply.github.com>
2021-09-12 01:27:20 +0530

    initial commit

---
+++ src/test/java/com/micro/ex/api/gateway/service/ApiGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.micro.ex.api.gateway.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
