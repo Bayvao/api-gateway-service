7ac0e02adbc4b05a75f0abb86325a0829e09abbe
Bayvao Verma <34921943+Bayvao@users.noreply.github.com>
2021-09-26 18:24:54 +0530

    routing configuration for registration and login service

--- src/main/java/com/micro/ex/api/gateway/service/GatewayConfig.java
+++ src/main/java/com/micro/ex/api/gateway/service/GatewayConfig.java
@@ -7,18 +7,36 @@ import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.micro.ex.api.gateway.service.filters.AuthorizationFilter;
+import com.micro.ex.api.gateway.service.filters.AuthorizationFilter.Config;
+
 @Configuration
 public class GatewayConfig {
-
+	
 	@Bean
-	public RouteLocator customRouteLocator(RouteLocatorBuilder routeBuilder) {
+	public RouteLocator customRouteLocator(RouteLocatorBuilder routeBuilder, AuthorizationFilter authFilter) {
 		return routeBuilder
-				.routes().route(r -> r
-						.path("/user-service/v1/users/status/check")
-						.and().header("Authorization", "Bearer .*")
+				.routes()
+				.route(r -> r
+						.path("/user-service/v1/users")
 						.filters(p -> p.rewritePath("/user-service/?(?<segment>.*)", "/${segment}")
 									.addResponseHeader("X-Response-Time", new Date().toString())
 									.removeRequestHeader("Cookie"))
+						.uri("lb://USER-SERVICE"))
+				.route(r -> r
+						.path("/user-service/users/login")
+						.filters(p -> p.rewritePath("/user-service/?(?<segment>.*)", "/${segment}")
+									.addResponseHeader("X-Response-Time", new Date().toString())
+									.removeRequestHeader("Cookie"))
+						.uri("lb://USER-SERVICE"))
+				
+				.route(r -> r
+						.path("/user-service/v1/users/**")
+						.and().header("Authorization", "Bearer .*")
+						.filters(p -> p.rewritePath("/user-service/?(?<segment>.*)", "/${segment}")
+									.addResponseHeader("X-Response-Time", new Date().toString())
+									.removeRequestHeader("Cookie")
+									.filter(authFilter.apply(new Config())))
 						.uri("lb://USER-SERVICE"))
 				.build();
 	}
